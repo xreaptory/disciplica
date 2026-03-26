4b1a3f27cb60d25b04d55eb4e33e202be0e0dbb1
Tekin Alp Yilmaz <yil240418@spengergasse.at>
2026-03-26 08:49:40 +0100

    MVC model import

---
+++ src/main/java/com/disciplica/bootstrap/HabitTrackerApp.java
@@ -0,0 +1,17 @@
+package com.disciplica.bootstrap;
+
+import View.View;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HabitTrackerApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        new View();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
